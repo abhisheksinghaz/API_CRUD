0b3b5f24b4c47d174139d477b85a9ed392c8f4d3
U-MAGICSW\abhishek.singh <abhishek.singh@axoni.com>
2022-01-14 13:19:14 +0530

    done till 6hrs 15mins

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
-import random
 from fastapi import FastAPI,Response,status,HTTPException, Depends
 from fastapi.params import Body
-from pydantic import BaseModel
-from pydantic.types import OptionalInt
-from random import randrange
-from starlette.status import HTTP_204_NO_CONTENT, HTTP_404_NOT_FOUND
-from . import models
+from typing import List
+from sqlalchemy.sql.functions import mode
+from starlette.status import HTTP_204_NO_CONTENT, HTTP_404_NOT_FOUND, HTTP_409_CONFLICT
+from . import models, utils
 from .database import SessionLocal,engine,get_db
 from sqlalchemy.orm import Session
+from . import schemas
+
 # v1\Scripts\activate               # to activate the virtual environment in cmd
 # uvicorn app.main:app --reload     # to run the server
 
@@ -30,48 +30,33 @@ except Exception as error:
     print("Error: ",error)
 ######################################################################################################
 
-class Post(BaseModel):
-    title: str
-    content: str
-    published: bool = False
+
     
 
 my_posts = [{"title":"T1","content":"I am positive content but still marked as False","id":1}]
-
-@app.get("/posts")
-def test_posts(db: Session = Depends(get_db)):
-    posts = db.query(models.Post).all()
-    return posts
-
-
-
-# @app.get("/posts")
-# def get_posts():
-#     cursor.execute("""SELECT * FROM posts """)
-#     posts = cursor.fetchall()
-#     return posts
 
 @app.get("/")
 def root():
     return [{"message": "Hello World"},{"message2":"I am message 2"}]
 
-@app.post("/posts",status_code=status.HTTP_201_CREATED)
-def create_posts(post: Post, db: Session = Depends(get_db)):
-    # cursor.execute("""INSERT INTO posts (title,content,published) values (%s, %s, %s) returning * """,
-    # (create_post_respone.title, create_post_respone.content, create_post_respone.published))
-    # new_post = cursor.fetchone()
-    # conn.commit()
+@app.get("/posts", response_model=List[schemas.PostResponse])
+def get_posts(db: Session = Depends(get_db)):
+    posts = db.query(models.Post).all()
+    return posts
+
+@app.post("/posts",status_code=status.HTTP_201_CREATED, response_model=schemas.PostResponse)
+def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
+
     new_post = models.Post(**post.dict())
     db.add(new_post)    # adding new post to the database object
     db.commit()         # commiting the newly made changes to the database.
     db.refresh(new_post)    # returning * -- type statement of SQL
     return new_post
 
-@app.get("/posts/{id}")
+@app.get("/posts/{id}", response_model=schemas.PostResponse)
 def get_post(id: int, db: Session = Depends(get_db)):
     post = db.query(models.Post).filter(models.Post.id == id).first()
-    # cursor.execute("""SELECT * FROM posts WHERE  id = {}""".format(id))
-    # post = cursor.fetchone()
+
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail="No post with id:{} exist in the database".format(id))
@@ -91,8 +76,8 @@ def delete_post(id: int, db: Session = Depends(get_db)):
     db.commit()
     return Response(status_code=HTTP_204_NO_CONTENT)
 
-@app.put("/posts/{id}")
-def update_post(id: int, post: Post, db: Session = Depends(get_db)):
+@app.put("/posts/{id}", response_model=schemas.PostResponse)
+def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db)):
     # 
     post_query = db.query(models.Post).filter(models.Post.id == id)
     # db.commit()
@@ -103,3 +88,33 @@ def update_post(id: int, post: Post, db: Session = Depends(get_db)):
     post_query.update(post.dict())
     db.commit()
     return post_query.first()
+
+@app.post("/users",status_code=status.HTTP_201_CREATED, response_model=schemas.UserCreateResponse)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+
+    # check if the email already exist.
+    user_exist = db.query(models.User).filter(models.User.email == user.email).first()
+    if user_exist:
+        raise HTTPException(status_code=HTTP_409_CONFLICT,
+        detail="User with email:{} already exist.".format(user.email))
+
+    #hash the password
+    #and then save this hashed password as the new password for the user
+    user.password = utils.hashing(user.password)
+
+    new_user = models.User(**user.dict())
+    db.add(new_user)    # adding new post to the database object
+    db.commit()         # commiting the newly made changes to the database.
+    db.refresh(new_user)    # returning * -- type statement of SQL
+    return new_user
+
+@app.get("/users/{id}",response_model=schemas.UserFetchByIdResponse)
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    # if the user does not exist
+    if not user :
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND, 
+        detail= "User with id:{} does not exist.".format(id))
+    
+    return user
+
